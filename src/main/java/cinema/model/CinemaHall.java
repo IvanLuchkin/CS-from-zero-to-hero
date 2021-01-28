482c9717faec09992151cc204d1d82e1f493655c
ivanluchkin <id.luchkin@gmail.com>
2021-01-28 20:04:51 +0200

    MovieSession and CinemaHall
    - DAO layer implemented
    - entities implemented
    - services implemented
    - mapping added to hibernate.cfg.xml
    - some tests added to Main.java

--- src/main/java/cinema/model/CinemaHall.java
+++ src/main/java/cinema/model/CinemaHall.java
@@ -8,21 +8,20 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "movies")
-public class Movie {
+@Table(name = "cinema_hall")
+public class CinemaHall {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
+    private int capacity;
     private String description;
 
-    public Movie(String title, String description) {
-        this.title = title;
-        this.description = description;
+    public CinemaHall() {
     }
 
-    public Movie() {
-
+    public CinemaHall(int capacity, String description) {
+        this.capacity = capacity;
+        this.description = description;
     }
 
     public Long getId() {
@@ -33,12 +32,12 @@ public class Movie {
         this.id = id;
     }
 
-    public String getTitle() {
-        return title;
+    public int getCapacity() {
+        return capacity;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
     }
 
     public String getDescription() {
@@ -51,9 +50,9 @@ public class Movie {
 
     @Override
     public String toString() {
-        return "Movie{"
+        return "CinemaHall{"
                 + "id=" + id
-                + ", title='" + title + '\''
+                + ", capacity=" + capacity
                 + ", description='" + description + '\''
                 + '}';
     }
@@ -66,13 +65,13 @@ public class Movie {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        Movie movie = (Movie) o;
-        return id.equals(movie.id) && title.equals(movie.title)
-                && description.equals(movie.description);
+        CinemaHall that = (CinemaHall) o;
+        return capacity == that.capacity && id.equals(that.id)
+                && description.equals(that.description);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, description);
+        return Objects.hash(id, capacity, description);
     }
 }
