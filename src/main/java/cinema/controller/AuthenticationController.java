298551004d4cf6a2f0e3239d4f5e6492d4b5bbe9
ivanluchkin <id.luchkin@gmail.com>
2021-02-16 17:16:44 +0200

    Fix: AuthController.register() now returns UserResponseDto

--- src/main/java/cinema/controller/AuthenticationController.java
+++ src/main/java/cinema/controller/AuthenticationController.java
@@ -1,7 +1,9 @@
 package cinema.controller;
 
 import cinema.model.dto.user.UserRequestDto;
+import cinema.model.dto.user.UserResponseDto;
 import cinema.security.AuthenticationService;
+import cinema.service.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,14 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthenticationController {
     private final AuthenticationService authService;
+    private final UserMapper userMapper;
 
     @Autowired
-    public AuthenticationController(AuthenticationService authService) {
+    public AuthenticationController(AuthenticationService authService, UserMapper userMapper) {
         this.authService = authService;
+        this.userMapper = userMapper;
     }
 
     @PostMapping("/register")
-    public void register(@RequestBody UserRequestDto userRequestDto) {
-        authService.register(userRequestDto.getEmail(), userRequestDto.getPassword());
+    public UserResponseDto register(@RequestBody UserRequestDto userRequestDto) {
+        return userMapper.mapToUserResponseDto(
+                authService.register(userRequestDto.getEmail(), userRequestDto.getPassword()));
     }
 }
